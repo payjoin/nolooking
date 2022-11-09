9fa83a49e11430b51528d0f991402573d4078437
DanGould <d@ngould.dev>
2022-11-09 10:34:41 -0500

    Give each request their own handler
    Co-authored-by: evanlinjin <hello@evanlinjin.me>

--- src/http.rs
+++ src/http.rs
@@ -1,4 +1,5 @@
 use std::net::SocketAddr;
+use std::path::Path;
 
 use bip78::receiver::*;
 use hyper::service::{make_service_fn, service_fn};
@@ -45,74 +46,78 @@ async fn handle_web_req(
     req: Request<Body>,
     endpoint: url::Url,
 ) -> Result<Response<Body>, hyper::Error> {
-    use std::path::Path;
-
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/pj") => {
-            let index =
-                std::fs::read(Path::new(STATIC_DIR).join("index.html")).expect("can't open index");
-            Ok(Response::new(Body::from(index)))
-        }
-
-        (&Method::GET, path) if path.starts_with("/pj/static/") => {
-            let directory_traversal_vulnerable_path = &path[("/pj/static/".len())..];
-            let file =
-                std::fs::read(Path::new(STATIC_DIR).join(directory_traversal_vulnerable_path))
-                    .expect("can't open static file");
-            Ok(Response::builder()
-                .status(200)
-                .header("Cache-Control", "max-age=604800")
-                .body(Body::from(file))
-                .expect("valid response"))
-        }
-
-        (&Method::POST, "/pj") => {
-            dbg!(req.uri().query());
-
-            let headers = Headers(req.headers().to_owned());
-            let query = {
-                let uri = req.uri();
-                if let Some(query) = uri.query() {
-                    Some(&query.to_owned());
-                }
-                None
-            };
-            let body = req.into_body();
-            let bytes = hyper::body::to_bytes(body).await?;
-            dbg!(&bytes); // this is correct by my accounts
-            let reader = &*bytes;
-            let original_request = UncheckedProposal::from_request(reader, query, headers).unwrap();
-
-            let proposal_psbt = scheduler.propose_payjoin(original_request).await.unwrap();
-
-            Ok(Response::new(Body::from(proposal_psbt)))
-        }
-
-        (&Method::POST, "/pj/schedule") => {
-            let bytes = hyper::body::to_bytes(req.into_body()).await?;
-            // deserialize x-www-form-urlencoded data with non-strict encoded "channel[arrayindex]"
-            let conf = serde_qs::Config::new(2, false);
-            let request: ScheduledPayJoin =
-                conf.deserialize_bytes(&bytes).expect("invalid request");
-
-            let address = scheduler.schedule_payjoin(&request).await.unwrap();
-            let total_amount = request.total_amount();
-            let uri = scheduler::format_bip21(address.clone(), total_amount, endpoint);
-            let mut response = Response::new(Body::from(uri.clone()));
-            create_qr_code(&uri, &address.to_string());
-            response
-                .headers_mut()
-                .insert(hyper::header::CONTENT_TYPE, "text/plain".parse().unwrap());
-            Ok(response)
-        }
-
-        // Return the 404 Not Found for other routes.
-        _ => {
-            let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
-        }
+        (&Method::GET, "/pj") => handle_index().await,
+        (&Method::GET, path) if path.starts_with("/pj/static/") => handle_static(path).await,
+        (&Method::POST, "/pj") => handle_pj(scheduler, req).await,
+        (&Method::POST, "/pj/schedule") => handle_pj_schedule(scheduler, endpoint, req).await,
+        _ => handle_404().await,
     }
+}
+
+async fn handle_404() -> Result<Response<Body>, hyper::Error> {
+    let mut not_found = Response::default();
+    *not_found.status_mut() = StatusCode::NOT_FOUND;
+    Ok(not_found)
+}
+
+async fn handle_index() -> Result<Response<Body>, hyper::Error> {
+    let index = std::fs::read(Path::new(STATIC_DIR).join("index.html")).expect("can't open index");
+    Ok(Response::new(Body::from(index)))
+}
+
+async fn handle_static(path: &str) -> Result<Response<Body>, hyper::Error> {
+    let directory_traversal_vulnerable_path = &path[("/pj/static/".len())..];
+    let file = std::fs::read(Path::new(STATIC_DIR).join(directory_traversal_vulnerable_path))
+        .expect("can't open static file");
+    Ok(Response::builder()
+        .status(200)
+        .header("Cache-Control", "max-age=604800")
+        .body(Body::from(file))
+        .expect("valid response"))
+}
+
+async fn handle_pj(
+    scheduler: Scheduler,
+    req: Request<Body>,
+) -> Result<Response<Body>, hyper::Error> {
+    dbg!(req.uri().query());
+
+    let headers = Headers(req.headers().to_owned());
+    let query = {
+        let uri = req.uri();
+        if let Some(query) = uri.query() {
+            Some(&query.to_owned());
+        }
+        None
+    };
+    let body = req.into_body();
+    let bytes = hyper::body::to_bytes(body).await?;
+    let reader = &*bytes;
+    let original_request = UncheckedProposal::from_request(reader, query, headers).unwrap();
+
+    let proposal_psbt = scheduler.propose_payjoin(original_request).await.unwrap();
+
+    Ok(Response::new(Body::from(proposal_psbt)))
+}
+
+async fn handle_pj_schedule(
+    scheduler: Scheduler,
+    endpoint: url::Url,
+    req: Request<Body>,
+) -> Result<Response<Body>, hyper::Error> {
+    let bytes = hyper::body::to_bytes(req.into_body()).await?;
+    // deserialize x-www-form-urlencoded data with non-strict encoded "channel[arrayindex]"
+    let conf = serde_qs::Config::new(2, false);
+    let request: ScheduledPayJoin = conf.deserialize_bytes(&bytes).expect("invalid request");
+
+    let address = scheduler.schedule_payjoin(&request).await.unwrap();
+    let total_amount = request.total_amount();
+    let uri = scheduler::format_bip21(address.clone(), total_amount, endpoint);
+    let mut response = Response::new(Body::from(uri.clone()));
+    create_qr_code(&uri, &address.to_string());
+    response.headers_mut().insert(hyper::header::CONTENT_TYPE, "text/plain".parse().unwrap());
+    Ok(response)
 }
 
 pub(crate) struct Headers(hyper::HeaderMap);
