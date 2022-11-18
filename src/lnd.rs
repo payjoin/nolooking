DanGould/loin

Make lsp client to request inbound capacity
When a users requests a quote for inbound channel capacity, the
lightning service provider can return an address to include in their
PayJoin PSBT with a price. By paying this address, the user can have
a channel opened to them paying from the same transaction they open
outbound channels.

src/lnd.rs
@@ -1,6 +1,7 @@
 use std::convert::TryFrom;
 use std::fmt;
 use std::num::TryFromIntError;
+use std::str::FromStr;
 use std::sync::Arc;
 
 use bitcoin::consensus::Decodable;
@@ -98,6 +99,17 @@ impl LndClient {
         response.get_ref().address.parse::<Address>().map_err(LndError::ParseBitcoinAddressFailed)
     }
 
+    pub async fn get_p2p_address(&self) -> Result<P2PAddress, LndError> {
+        let mut client = self.0.lock().await;
+        let response = client
+            .lightning()
+            .get_info(tonic_lnd::lnrpc::GetInfoRequest { ..Default::default() })
+            .await?;
+        let node_pubkey = P2PAddress::from_str(&response.into_inner().uris[0])
+            .map_err(LndError::ParseP2PAddressFailed)?;
+        Ok(node_pubkey)
+    }
+
     /// Requests to open a channel with remote node, returning the psbt of the funding transaction.
     pub async fn open_channel(
         &self,
@@ -185,6 +197,7 @@ pub enum LndError {
     Decode(bitcoin::consensus::encode::Error),
     ParseBitcoinAddressFailed(bitcoin::util::address::Error),
     ParseReserveAsSatFailed(TryFromIntError),
+    ParseP2PAddressFailed(ln_types::p2p_address::ParseError),
     VersionRequestFailed(tonic_lnd::Error),
     UnexpectedUpdate(tonic_lnd::lnrpc::open_status_update::Update),
     ParseVersionFailed { version: String, error: std::num::ParseIntError },
@@ -199,6 +212,7 @@ impl fmt::Display for LndError {
             LndError::Decode(e) => e.fmt(f),
             LndError::ParseBitcoinAddressFailed(e) => e.fmt(f),
             LndError::ParseReserveAsSatFailed(err) => err.fmt(f),
+            LndError::ParseP2PAddressFailed(e) => e.fmt(f),
             LndError::VersionRequestFailed(_) => write!(f, "failed to get LND version"),
             LndError::UnexpectedUpdate(e) => write!(f, "Unexpected channel update {:?}", e),
             LndError::ParseVersionFailed { version, error: _ } => {
@@ -221,6 +235,7 @@ impl std::error::Error for LndError {
             LndError::Decode(e) => Some(e),
             LndError::ParseBitcoinAddressFailed(e) => Some(e),
             LndError::ParseReserveAsSatFailed(_) => None,
+            LndError::ParseP2PAddressFailed(e) => Some(e),
             LndError::VersionRequestFailed(e) => Some(e),
             Self::UnexpectedUpdate(_) => None,
             LndError::ParseVersionFailed { version: _, error } => Some(error),

src/scheduler.rs
@@ -13,6 +13,7 @@ use url::Url;
 
 use crate::args::ArgError;
 use crate::lnd::{LndClient, LndError};
+use crate::lsp::LspError;
 
 #[derive(Clone, serde_derive::Deserialize, Debug)]
 pub struct ScheduledChannel {
@@ -37,16 +38,17 @@ impl ScheduledChannel {
 #[derive(Clone, serde_derive::Deserialize, Debug)]
 pub struct ChannelBatch {
     channels: Vec<ScheduledChannel>,
+    wants_inbound_quote: bool,
     fee_rate: u64,
 }
 
 impl ChannelBatch {
-    pub fn new(channels: Vec<ScheduledChannel>, fee_rate: u64) -> Self {
-        Self { channels, fee_rate }
+    pub fn new(channels: Vec<ScheduledChannel>, wants_inbound_quote: bool, fee_rate: u64) -> Self {
+        Self { channels, wants_inbound_quote, fee_rate }
     }
 
     pub fn channels(&self) -> &Vec<ScheduledChannel> { &self.channels }
-
+    pub fn wants_inbound_quote(&self) -> bool { self.wants_inbound_quote }
     pub fn fee_rate(&self) -> u64 { self.fee_rate }
 }
 
@@ -58,11 +60,21 @@ pub struct ScheduledPayJoin {
     wallet_amount: bitcoin::Amount,
     channels: Vec<ScheduledChannel>,
     fee_rate: u64,
+    quote: Option<crate::lsp::Quote>,
 }
 
 impl ScheduledPayJoin {
-    pub fn new(wallet_amount: bitcoin::Amount, batch: ChannelBatch) -> Self {
-        Self { wallet_amount, channels: batch.channels().clone(), fee_rate: batch.fee_rate() }
+    pub fn new(
+        wallet_amount: bitcoin::Amount,
+        batch: ChannelBatch,
+        quote: Option<crate::lsp::Quote>,
+    ) -> Self {
+        Self {
+            wallet_amount,
+            channels: batch.channels().clone(),
+            fee_rate: batch.fee_rate(),
+            quote,
+        }
     }
 
     fn total_amount(&self) -> bitcoin::Amount {
@@ -240,6 +252,8 @@ impl Scheduler {
         Self { lnd, endpoint, pjs: Default::default() }
     }
 
+    pub fn lnd(&self) -> &LndClient { &self.lnd }
+
     pub async fn from_config(config: &crate::config::Config) -> Result<Self, SchedulerError> {
         Ok(Scheduler::new(LndClient::from_config(&config).await?, config.endpoint.parse().expect("Malformed secure endpoint from config file. Expecting a https or .onion URI to proxy payjoin requests")))
     }
@@ -254,7 +268,15 @@ impl Scheduler {
         let bitcoin_addr = self.lnd.get_new_bech32_address().await?;
 
         let required_reserve = self.lnd.required_reserve(batch.channels().len() as u32).await?;
-        let pj = &ScheduledPayJoin::new(required_reserve, batch);
+        let inbound_quote = if batch.wants_inbound_quote() {
+            match self.request_quote().await {
+                Ok(quote) => Some(quote),
+                Err(e) => return Err(e),
+            }
+        } else {
+            None
+        };
+        let pj = &ScheduledPayJoin::new(required_reserve, batch, inbound_quote.clone());
 
         if self.insert_payjoin(&bitcoin_addr, pj) {
             Ok((
@@ -264,6 +286,17 @@ impl Scheduler {
         } else {
             Err(SchedulerError::Internal("lnd provided duplicate bitcoin addresses"))
         }
+    }
+
+    /// Get a quote for an inbound channel from the nolooking service.
+    /// If the service is unavailable, just return None.
+    async fn request_quote(&self) -> Result<crate::lsp::Quote, SchedulerError> {
+        let node_pubkey = self.lnd.get_p2p_address().await?;
+        let refund_address = self.lnd.get_new_bech32_address().await?;
+        let quote = crate::lsp::request_quote(&node_pubkey, &refund_address)
+            .await
+            .map_err(SchedulerError::Lsp)?;
+        Ok(quote)
     }
 
     /// Given an Original PSBT request, respond with a PayJoin Proposal,
@@ -390,6 +423,9 @@ pub fn format_bip21(address: Address, amount: Amount, endpoint: url::Url) -> Str
 
 #[derive(Debug)]
 pub enum SchedulerError {
+    /// Error at the lightning service provider controller
+    Lsp(LspError),
+    /// Error at the lightning node controller
     Lnd(LndError),
     /// Internal error that should not be shared
     Internal(&'static str),

payjoin/nolooking

src/lnd.rs
@@ -1,6 +1,7 @@
 use std::convert::TryFrom;
 use std::fmt;
 use std::num::TryFromIntError;
+use std::str::FromStr;
 use std::sync::Arc;
 
 use bitcoin::consensus::Decodable;
@@ -98,6 +99,17 @@ impl LndClient {
         response.get_ref().address.parse::<Address>().map_err(LndError::ParseBitcoinAddressFailed)
     }
 
+    pub async fn get_p2p_address(&self) -> Result<P2PAddress, LndError> {
+        let mut client = self.0.lock().await;
+        let response = client
+            .lightning()
+            .get_info(tonic_lnd::lnrpc::GetInfoRequest { ..Default::default() })
+            .await?;
+        let p2p_address = P2PAddress::from_str(&response.into_inner().uris[0])
+            .map_err(LndError::ParseP2PAddressFailed)?;
+        Ok(p2p_address)
+    }
+
     /// Requests to open a channel with remote node, returning the psbt of the funding transaction.
     pub async fn open_channel(
         &self,
@@ -185,6 +197,7 @@ pub enum LndError {
     Decode(bitcoin::consensus::encode::Error),
     ParseBitcoinAddressFailed(bitcoin::util::address::Error),
     ParseReserveAsSatFailed(TryFromIntError),
+    ParseP2PAddressFailed(ln_types::p2p_address::ParseError),
     VersionRequestFailed(tonic_lnd::Error),
     UnexpectedUpdate(tonic_lnd::lnrpc::open_status_update::Update),
     ParseVersionFailed { version: String, error: std::num::ParseIntError },
@@ -199,6 +212,7 @@ impl fmt::Display for LndError {
             LndError::Decode(e) => e.fmt(f),
             LndError::ParseBitcoinAddressFailed(e) => e.fmt(f),
             LndError::ParseReserveAsSatFailed(err) => err.fmt(f),
+            LndError::ParseP2PAddressFailed(e) => e.fmt(f),
             LndError::VersionRequestFailed(_) => write!(f, "failed to get LND version"),
             LndError::UnexpectedUpdate(e) => write!(f, "Unexpected channel update {:?}", e),
             LndError::ParseVersionFailed { version, error: _ } => {
@@ -221,6 +235,7 @@ impl std::error::Error for LndError {
             LndError::Decode(e) => Some(e),
             LndError::ParseBitcoinAddressFailed(e) => Some(e),
             LndError::ParseReserveAsSatFailed(_) => None,
+            LndError::ParseP2PAddressFailed(e) => Some(e),
             LndError::VersionRequestFailed(e) => Some(e),
             Self::UnexpectedUpdate(_) => None,
             LndError::ParseVersionFailed { version: _, error } => Some(error),

src/scheduler.rs
@@ -13,6 +13,7 @@ use url::Url;
 
 use crate::args::ArgError;
 use crate::lnd::{LndClient, LndError};
+use crate::lsp::LspError;
 
 #[derive(Clone, serde_derive::Deserialize, Debug)]
 pub struct ScheduledChannel {
@@ -37,16 +38,17 @@ impl ScheduledChannel {
 #[derive(Clone, serde_derive::Deserialize, Debug)]
 pub struct ChannelBatch {
     channels: Vec<ScheduledChannel>,
+    wants_inbound_quote: bool,
     fee_rate: u64,
 }
 
 impl ChannelBatch {
-    pub fn new(channels: Vec<ScheduledChannel>, fee_rate: u64) -> Self {
-        Self { channels, fee_rate }
+    pub fn new(channels: Vec<ScheduledChannel>, wants_inbound_quote: bool, fee_rate: u64) -> Self {
+        Self { channels, wants_inbound_quote, fee_rate }
     }
 
     pub fn channels(&self) -> &Vec<ScheduledChannel> { &self.channels }
-
+    pub fn wants_inbound_quote(&self) -> bool { self.wants_inbound_quote }
     pub fn fee_rate(&self) -> u64 { self.fee_rate }
 }
 
@@ -58,11 +60,21 @@ pub struct ScheduledPayJoin {
     wallet_amount: bitcoin::Amount,
     channels: Vec<ScheduledChannel>,
     fee_rate: u64,
+    quote: Option<crate::lsp::Quote>,
 }
 
 impl ScheduledPayJoin {
-    pub fn new(wallet_amount: bitcoin::Amount, batch: ChannelBatch) -> Self {
-        Self { wallet_amount, channels: batch.channels().clone(), fee_rate: batch.fee_rate() }
+    pub fn new(
+        wallet_amount: bitcoin::Amount,
+        batch: ChannelBatch,
+        quote: Option<crate::lsp::Quote>,
+    ) -> Self {
+        Self {
+            wallet_amount,
+            channels: batch.channels().clone(),
+            fee_rate: batch.fee_rate(),
+            quote,
+        }
     }
 
     fn total_amount(&self) -> bitcoin::Amount {
@@ -254,7 +266,15 @@ impl Scheduler {
         let bitcoin_addr = self.lnd.get_new_bech32_address().await?;
 
         let required_reserve = self.lnd.required_reserve(batch.channels().len() as u32).await?;
-        let pj = &ScheduledPayJoin::new(required_reserve, batch);
+        let inbound_quote = if batch.wants_inbound_quote() {
+            match self.request_quote().await {
+                Ok(quote) => Some(quote),
+                Err(e) => return Err(e),
+            }
+        } else {
+            None
+        };
+        let pj = &ScheduledPayJoin::new(required_reserve, batch, inbound_quote.clone());
 
         if self.insert_payjoin(&bitcoin_addr, pj) {
             Ok((
@@ -264,6 +284,17 @@ impl Scheduler {
         } else {
             Err(SchedulerError::Internal("lnd provided duplicate bitcoin addresses"))
         }
+    }
+
+    /// Get a quote for an inbound channel from the nolooking service.
+    /// If the service is unavailable, just return None.
+    async fn request_quote(&self) -> Result<crate::lsp::Quote, SchedulerError> {
+        let p2p_address = self.lnd.get_p2p_address().await?;
+        let refund_address = self.lnd.get_new_bech32_address().await?;
+        let quote = crate::lsp::request_quote(&p2p_address, &refund_address)
+            .await
+            .map_err(SchedulerError::Lsp)?;
+        Ok(quote)
     }
 
     /// Given an Original PSBT request, respond with a PayJoin Proposal,
@@ -390,6 +421,9 @@ pub fn format_bip21(address: Address, amount: Amount, endpoint: url::Url) -> Str
 
 #[derive(Debug)]
 pub enum SchedulerError {
+    /// Error at the lightning service provider controller
+    Lsp(LspError),
+    /// Error at the lightning node controller
     Lnd(LndError),
     /// Internal error that should not be shared
     Internal(&'static str),